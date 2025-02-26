24059d22ed03921f87e647ff024f2e09b83798fb
Chelsea Guthridge (Information Technology) <e20249012@dekalbschoolsga.org>
2025-02-26 13:53:15 -0500

    Initial commit - Added fading text animation, theme toggle, and color picker

---
+++ text_animation/lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool isDarkMode = false;
+
+  void toggleTheme() {
+    setState(() {
+      isDarkMode = !isDarkMode;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: HomeScreen(toggleTheme: toggleTheme, isDarkMode: isDarkMode),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  final VoidCallback toggleTheme;
+  final bool isDarkMode;
+
+  HomeScreen({required this.toggleTheme, required this.isDarkMode});
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  bool _isVisible = true;
+  Color _textColor = Colors.blue;
+
+  void toggleVisibility() {
+    setState(() {
+      _isVisible = !_isVisible;
+    });
+  }
+
+  void changeTextColor() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Pick a color"),
+          content: SingleChildScrollView(
+            child: ColorPicker(
+              pickerColor: _textColor,
+              onColorChanged: (color) {
+                setState(() {
+                  _textColor = color;
+                });
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("Close"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Fading Text Animation'),
+        actions: [
+          IconButton(
+            icon: Icon(widget.isDarkMode ? Icons.nightlight_round : Icons.wb_sunny),
+            onPressed: widget.toggleTheme,
+          ),
+          IconButton(
+            icon: Icon(Icons.palette),
+            onPressed: changeTextColor,
+          ),
+        ],
+      ),
+      body: Center(
+        child: AnimatedOpacity(
+          opacity: _isVisible ? 1.0 : 0.0,
+          duration: Duration(seconds: 1),
+          child: Text(
+            'Hello, Flutter!',
+            style: TextStyle(fontSize: 24, color: _textColor),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: toggleVisibility,
+        child: Icon(Icons.play_arrow),
+      ),
+    );
+  }
+}
